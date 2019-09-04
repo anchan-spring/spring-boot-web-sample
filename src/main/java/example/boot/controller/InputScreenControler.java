7797c268a03152e6e3ba022096028aa6db1e5cf6
kcc0941 <kcc0941@NPC-18-0048.kcc-hsc.internal>
2019-09-04 20:01:56 +0900

    feat:入力画面から確認画面

---
+++ src/main/java/example/boot/controller/InputScreenControler.java
@@ -0,0 +1,34 @@
+package example.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import example.boot.entity.InputEntity;
+
+@Controller
+public class InputScreenControler {
+
+  @RequestMapping("/input")
+  public String showInputScreen(Model model) {
+	model.addAttribute("InputEntity", new InputEntity());
+
+	// ビュー名として"hello"を返却
+	// Viewは下記となる
+	// src/main/resources/templates/hello.html
+	return "input";
+  }
+
+  @RequestMapping(value = "/confirm", method = RequestMethod.POST)
+  public String confirm(@ModelAttribute("InputEntity") InputEntity form) {
+	// 遷移先を返す(この場合はconfirm.htmlが遷移先となる)
+	return "confirm";
+  }
+
+  @RequestMapping(value = "edit", params = "doBack", method = RequestMethod.POST)
+  public String doBack() {
+	  return "input";
+  }
+}
